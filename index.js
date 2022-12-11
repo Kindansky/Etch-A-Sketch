40cbb8d7f2ae8efd980553e7023e7367ae305ddc
Kindansky <118498827+Kindansky@users.noreply.github.com>
2022-12-11 13:22:35 +0100

    Add rainbow color

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const container = document.getElementById("grid");
 const button = document.querySelector("#confirm");
 const input = document.querySelector("#number");
+const rainbow = document.querySelector("#rainbow");
+const clear = document.querySelector("#clear");
 
+function getInput() {
+    const value = document.querySelectorAll('.grid-item').value;
+    console.log(value);
+  }
 function makeRows(v) {
     container.innerHTML = '';
     container.style.setProperty('--grid-rows', v);
@@ -11,6 +17,7 @@ function makeRows(v) {
         cell.style.width = "1/v %";
         cell.style.height = "1/v %";
         container.appendChild(cell).className = "grid-item";
+
         let randomColor = Math.floor(Math.random()*16777215).toString(16);
         cell.onmouseover = () => cell.style.backgroundColor = "#" + randomColor;
 
@@ -18,3 +25,11 @@ function makeRows(v) {
 };
 
 button.addEventListener("click", () => makeRows(input.value));
+clear.addEventListener("click", () => makeRows(input.value));
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+let cells = document.querySelectorAll(".grid-item");
+
+//rainbow.addEventListener("click", () => )
+for (let i = 0; i < cells.length; i++) {
+    cells[i].onmouseover = () => cells[i].style.backgroundColor = "#" + randomColor;
+};
